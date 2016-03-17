9280625f725928ac72dfa833f8cd7584a5cfdaff
oreHGA <oreogundipe@gmail.com>
2016-03-17 16:20:24 +0000

    Assignment COmplete

---
+++ src/CountingByHalves_Q68.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nana Kofi
+ */
+public class CountingByHalves_Q68 {
+    public static void main(String[] args){
+        for (float x = -10; x<= 10;x+= 0.5){
+            System.out.println(x);
+        }
+    }
+}
